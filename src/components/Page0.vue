472ac1a0b58a91f4cea0ed611cb90ecbbcd699b0
esperandote <unnino@126.com>
2022-07-30 07:44:26 +0800

    feat: update apk

--- src/components/Page0.vue
+++ src/components/Page0.vue
@@ -39,7 +39,7 @@ export default {
   },
   methods: {
     goToMarket() {
-      window.open("./starmystery-release-v1.0.0-1390095.apk")
+      window.open("./starmystery-release-v1.0.0-fd971dc.apk")
     }
   }
 }
